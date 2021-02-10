41a88bc76542c4cd5a51001188afb6c6c79fafa0
Ilkana B. Alizada <IlkanaBa@CODE.LOCAL>
2021-02-10 14:10:04 +0400

    homework

---
+++ index.js
@@ -0,0 +1,56 @@
+Highcharts.chart('container', {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: 0,
+      plotShadow: false
+    },
+    title: {
+      text: 'My<br>Favorite<br>2021',
+      align: 'center',
+      verticalAlign: 'middle',
+      y: 60
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+      point: {
+        valueSuffix: '%'
+      }
+    },
+    plotOptions: {
+      pie: {
+        dataLabels: {
+          enabled: true,
+          distance: -50,
+          style: {
+            fontWeight: 'bold',
+            color: 'white'
+          }
+        },
+        startAngle: -90,
+        endAngle: 90,
+        center: ['50%', '75%'],
+        size: '110%'
+      }
+    },
+    series: [{
+      type: 'pie',
+      name: 'Browser share',
+      innerSize: '50%',
+      data: [
+        ['HardWork', 58.9],
+        ['Books', 58.9],
+        ['Drink tea', 13],
+        ['Musics', 3.78],
+        ['Dances', 3.42],
+        {
+          name: 'Other',
+          y: 7.61,
+          dataLabels: {
+            enabled: false
+          }
+        }
+      ]
+    }]
+  });
